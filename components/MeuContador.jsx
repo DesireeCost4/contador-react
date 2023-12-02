91fca85be83190a42eb324783331dc94c74d5a77
Desireê Costa <desireec.dev@gmail.com>
2023-12-02 20:42:40 -0300

    aplicando mudança de estado

--- components/MeuContador.jsx
+++ components/MeuContador.jsx
@@ -1,15 +1,15 @@
-//01 aqui crio o componente no caso MeuContador e em seguida eu declaro o que será mostrado no return
+
+
 import { useState } from "react" 
 
 export default function MeuContador() {
 
-   let contador = 0 //defini variavel dentro do componente criado
+   console.log ('contando')
 
+   const [contador, setContador] = useState(0)
 
-   function aumentar () { //criei uma função para aumentar o numero da variavel a cada clique
-    contador ++            // evendo definido no onclick, a cada clique a variavel soma 1
-    console.log('o valor é', contador)
-
+   function aumentar () { 
+    setContador(contador + 1)
    }
 
     return (
